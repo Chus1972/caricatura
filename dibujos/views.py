feaf822c485ae1293e07b4a4290c4299d6c61cff
caricaturas <caricaturas@MacBook-Pro-de-barcelona.local>
2015-05-03 23:06:54 +0200

    subida desde pro

--- dibujos/views.py
+++ dibujos/views.py
@@ -43,7 +43,7 @@ def submit_form(request):
 	c.update(csrf(request))
 	print c
 
-	username = request.GET.get("username")
+	username = request.POST.post("username")
 	full_name = request.GET.get("full_name")
 	avatar_url = request.GET.get("avatar_url")
 
