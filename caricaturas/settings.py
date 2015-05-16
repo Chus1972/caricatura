5554edbc12822709603a6ce47b9fa267f7f4f8ce
caricaturas <caricaturas@MacBook-Pro-de-barcelona.local>
2015-05-16 12:30:08 +0200

    subida desde pro

--- caricaturas/settings.py
+++ caricaturas/settings.py
@@ -108,11 +108,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 #STATIC_ROOT = ('content',)
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-    
+#STATICFILES_DIRS = (
+ #   os.path.join(BASE_DIR, 'static'),
+ #   
 
-)
+#)
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
