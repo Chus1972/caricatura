0112961702ac71f70541973e5d6ee3f80ebeb630
caricaturas <caricaturas@MacBook-Pro-de-barcelona.local>
2015-06-21 11:40:04 +0200

    subida desde pro

--- dibujos/crud_caricaturas.py
+++ dibujos/crud_caricaturas.py
@@ -3,24 +3,7 @@ from django.http import HttpResponse
 import json, os
 import datetime
 
-<<<<<<< HEAD
-def alta_caricatura(request, idArtista, titulo, tag, imgAlta, imgBaja):
-	dicc = {}
-	try:
-		caricatura = Caricaturas.objects.get(idartista = idArtista, titulo = titulo)
-	except Caricaturas.DoesNotExist: # Caricatura no existe y se puede crear
-		print 'entra'
-		artista = Artista.objects.get(id = idArtista)
-		caricatura = Caricaturas(idartista = artista, titulo = titulo, tag = tag, img_alta = imgAlta, 
-								 img_miniatura = imgBaja, 
-								 fechasubida = datetime.datetime.now() + datetime.timedelta(hours = 2),
-								 facebook = 0, twitter = 0, googleplus = 0)
-		print 'por aqui pasa'
-		caricatura.save()
 
-		dicc = {'content' : 'OK', 'mensaje' : {'idartista' : caricatura.idartista.id, 'titulo' : titulo}}
-	else: # la caricatura ya existe
-=======
 def alta_caricatura(request, idArtista, titulo, tag, imgAlta, imgBaja, facebook, twitter, googleplus):
 	dicc = {}
 	try:
@@ -34,12 +17,11 @@ def alta_caricatura(request, idArtista, titulo, tag, imgAlta, imgBaja, facebook,
 		print 'grabado'
 		dicc = {'content' : 'OK', 'mensaje' : {'idArtista' : idartista, 'titulo' : titulo, 'imgAlta' : imgAlta, 'imgBaja' : imgBaja}}
 	else: # el usuario ya existe
->>>>>>> 113b6982516190748b66154962a162c304f3a64d
 		dicc = {'content' : 'KO', 'mensaje' : {'error' : 'Esta caricatura ya existe'}}
 	data = '%s(%s);' % (request.GET.get('callback'), json.dumps(dicc))
 	return HttpResponse(data, 'application/json')
 
-<<<<<<< HEAD
+
 def borrar_caricatura(request, idartista, titulo):
 	dicc = {}
 	try:
@@ -51,7 +33,7 @@ def borrar_caricatura(request, idartista, titulo):
 	except Caricaturas.DoesNotExist: # Caricatura no existe y no puede ser borrado
 		artista = Artista.objects.get(id = idartista)
 		dicc = {'content' : 'KO', 'mensaje' :  'La caricatura %s del artista %s %s no existe.' % (titulo, artista.nombre, artista.apellidos )}
-=======
+
 def borrar_caricatura(request, idArtista, titulo):
 	dicc = {}
 	try:
@@ -61,12 +43,10 @@ def borrar_caricatura(request, idArtista, titulo):
 
 	except Caricaturas.DoesNotExist: # Caricatura no existe y no puede ser borrada
 		dicc = {'content' : 'KO', 'mensaje' :  'La caricatura %s del artista %s no existe.' % (titulo, idArtista)}
->>>>>>> 113b6982516190748b66154962a162c304f3a64d
 
 	data = '%s(%s);' % (request.GET.get('callback'), json.dumps(dicc))
 	return HttpResponse(data, 'application/json')
 
-<<<<<<< HEAD
 def update_caricatura(request, idartista, titulo, nuevo_titulo, tag, img_alta, img_miniatura):
 	dicc = {}
 	try:
@@ -86,7 +66,3 @@ def update_caricatura(request, idartista, titulo, nuevo_titulo, tag, img_alta, i
 	data = '%s(%s);' % (request.GET.get('callback'), json.dumps(dicc))
 	return HttpResponse(data, 'application/json')
 
-=======
-
-
->>>>>>> 113b6982516190748b66154962a162c304f3a64d
