4bba042c20fbe80a94154185d37008b0e43b4d5e
caricatura <caricatura@Mac-mini-de-Jesus.local>
2015-07-17 18:06:13 +0200

    subida desde mini

--- dibujos/views.py
+++ dibujos/views.py
@@ -109,11 +109,10 @@ def artistas(request):
 def subir_s3(request):
 
 	print 'Entra subir_s3'
-	print 'POST'
-	print 'fichero'
+
 	print request.POST['filename']
 	if request.method == 'POST': # Esto quiere decir que se han llenado los datos del formulario
-		
+		print 'entra'
 		nombre_fichero = request.POST.get('filename')
 		# Hace la subida del fichero a s3
 		con_s3 = S3Connection('AKIAJNC4CIHRDOPQTENQ', 'X6u5N8Kc+TGuWxdIk9BK3xJXzcIOPTx6BpvGI7uH')
