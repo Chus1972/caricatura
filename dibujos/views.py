b19e56d594d259a5c6a27be2963471cb354df8a2
caricaturas <caricaturas@MacBook-Pro-de-barcelona.local>
2015-07-17 20:01:17 +0200

    subida desde pro

--- dibujos/views.py
+++ dibujos/views.py
@@ -109,7 +109,7 @@ def artistas(request):
 def subir_s3(request):
 
 	print 'Entra subir_s3'
-	print request.POST['filename']
+	print REQUEST['filename']
 	if request.method == 'POST': # Esto quiere decir que se han llenado los datos del formulario
 		print 'entra'
 		nombre_fichero = request.POST.get('filename')
